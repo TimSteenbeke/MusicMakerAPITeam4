1d7a23b0f0875665f588096c9be3e1c352c4a4a7
benvanraemdonck <ben.vanraemdonck@student.kdg.be>
2018-02-20 15:38:19 +0100

    CORS added

--- src/main/java/be/kdg/ip/web/InstrumentController.java
+++ src/main/java/be/kdg/ip/web/InstrumentController.java
@@ -15,6 +15,7 @@ import javax.validation.Valid;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/instruments")
 public class InstrumentController {
     private InstrumentService instrumentService;
