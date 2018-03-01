0f2388da527e24cb0f5418c71bd3f25f79257486
TimSteenbeke <tim.steenbeke@student.kdg.be>
2018-03-01 22:13:11 +0100

    import cleanup logincontroller

--- src/main/java/be/kdg/ip/web/LoginController.java
+++ src/main/java/be/kdg/ip/web/LoginController.java
@@ -1,18 +1,8 @@
 package be.kdg.ip.web;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import sun.net.www.http.HttpClient;
 
-import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.nio.charset.Charset;
 
 @RestController
 @CrossOrigin(origins = "*")
