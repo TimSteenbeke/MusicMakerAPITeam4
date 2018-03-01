31cadaed9898ee75cec05d6c81008933c7a08ec6
Michiel Bervoets <michiel.bervoets@student.kdg.be>
2018-03-01 14:11:32 +0100

    Muziek stukken uploaden backend

--- src/main/java/be/kdg/ip/services/impl/Initializer.java
+++ src/main/java/be/kdg/ip/services/impl/Initializer.java
@@ -48,7 +48,6 @@ public class Initializer {
     @Autowired
     private GroupService groupService;
 
-
     @PostConstruct
     public void addDummyInstruments() {
 
@@ -68,7 +67,6 @@ public class Initializer {
         instrumentService.addInstrument(instrument2);
         instrumentService.addInstrument(instrument3);
     }
-
 
     @PostConstruct
     public void addDummyUser() throws UserServiceException {
