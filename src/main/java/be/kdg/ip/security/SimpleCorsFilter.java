c384ea19a45821d748ece3ae1ef255756f80bc7c
TimSteenbeke <tim.steenbeke@student.kdg.be>
2018-03-16 14:26:47 +0100

    working socket

--- src/main/java/be/kdg/ip/security/SimpleCorsFilter.java
+++ src/main/java/be/kdg/ip/security/SimpleCorsFilter.java
@@ -9,10 +9,16 @@ import org.springframework.stereotype.Component;
 import javax.servlet.*;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
 class SimpleCorsFilter implements Filter {
+
+    private final List<String> allowedOrigins = Arrays.asList("http://localhost:3000",
+            "http://musicmaker-team4.herokuapp.com",
+            "https://musicmaker-team4.herokuapp.com");
 
     public SimpleCorsFilter() {
     }
@@ -21,10 +27,18 @@ class SimpleCorsFilter implements Filter {
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
         HttpServletRequest request = (HttpServletRequest) req;
-        response.setHeader("Access-Control-Allow-Origin", "*");
+
+        String origin = request.getHeader("Origin");
+
+        response.setHeader("Access-Control-Allow-Origin", allowedOrigins.contains(origin) ? origin : "");
+
+        response.setHeader("Vary", "Origin");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+
         response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "x-requested-with, authorization, cache-control, Content-Type");
+
 
         if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
             response.setStatus(HttpServletResponse.SC_OK);
