0a1ebf51bc7fc2281255d72cc525f9715c24ab78
TimSteenbeke <tim.steenbeke@student.kdg.be>
2018-03-16 22:46:38 +0100

    final changes for master push

--- src/main/java/be/kdg/ip/Chat/WebSocketController.java
+++ src/main/java/be/kdg/ip/Chat/WebSocketController.java
@@ -1,10 +1,8 @@
 package be.kdg.ip.Chat;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,11 +22,6 @@ public class WebSocketController {
 
     @MessageMapping("/send/message/{chatroom}")
     public void onRecieveMessage(@PathVariable("chatroom") String chatroom, String message) {
-        System.out.println("========== ========== ========== ========== ========== ========== ==========");
-        System.out.println("/send/message/{chatroom}");
-        System.out.println("id: " + chatroom);
-        System.out.println("message: " + message);
-        System.out.println("========== ========== ========== ========== ========== ========== ==========");
         this.template.convertAndSend("/chat/" + chatroom,
                 new SimpleDateFormat("HH:mm:ss").format(new Date()) + "- " + message);
     }
