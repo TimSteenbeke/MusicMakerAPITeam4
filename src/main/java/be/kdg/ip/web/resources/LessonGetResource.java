91ccf809e186cf9be947d26b38a2def64d0d0764
Jari Van Meirvenne <jarivmeirvenne@gmail.com>
2018-03-20 19:39:26 +0100

    Id fix lesson

--- src/main/java/be/kdg/ip/web/resources/LessonGetResource.java
+++ src/main/java/be/kdg/ip/web/resources/LessonGetResource.java
@@ -16,6 +16,15 @@ public class LessonGetResource {
     @JsonSerialize(using = LocalDateTimeSerializer.class)
     private LocalDateTime enddatetime;
     private Course course;
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
     public LocalDateTime getStartdatetime() {
         return startdatetime;
